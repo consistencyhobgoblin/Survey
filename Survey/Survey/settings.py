2ccc7a7967c8e9f7c3061b8b022e84cd91e4cb81
Matt Owen <consistencyhobgoblin-github@yahoo.com>
2020-01-13 14:37:29 -0800

    Initial project creation, switch to Postgres.

--- Survey/Survey/settings.py
+++ Survey/Survey/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = 'Survey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'OPTIONS': {
+            'options': '-c search_path=survey'
+            },
+        'NAME': 'Survey',
+        'USER': 'ds_ops',
+        'PASSWORD': '@dMinistr8',
+        'HOST': '184.169.226.219',
+        'PORT': '5432',
     }
 }
 
